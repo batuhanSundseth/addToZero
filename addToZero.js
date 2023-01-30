7d3408d4cdec23035f837ca370c73ffee0fb1774
Batuhan Sundseth <batuhansundseth@pm.me>
2023-01-30 10:06:39 -0500

    No longer returns true every time

--- addToZero.js
+++ addToZero.js
@@ -1,8 +1,8 @@
-let array = [1, 4, 11, 2, 37, -4]
+let array = [0, 1, 2, 3, 4, 5]
 let addToZero = false
 for (let i = 0; i < array.length; i++) {
   for (let j = i; j < array.length; j++) {
-    if (array[i] + array[j] == 0) {
+    if (array[i] + array[j + 1] == 0) {
       addToZero = true
     }
   }
