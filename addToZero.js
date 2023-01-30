5242f922c58084bc329bcd29dd2c6ecf77655cdd
Batuhan Sundseth <batuhansundseth@pm.me>
2023-01-30 11:45:53 -0500

    Refactored code

--- addToZero.js
+++ addToZero.js
@@ -1,10 +1,11 @@
 let array = [0, 1, 2, 3, 4, 5]
-let addToZero = false
-for (let i = 0; i < array.length; i++) {
-  for (let j = i; j < array.length; j++) {
-    if (array[i] + array[j + 1] == 0) {
-      addToZero = true
+let addToZero = arr => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] == 0)
+        return true
     }
   }
+  return false
 }
-console.log(addToZero)
+console.log(addToZero(array))
